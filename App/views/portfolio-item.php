b8e888080416012893653c7782dd6a60404d3945
AinhoaGV <ainhoagomez@gmail.com>
2026-05-20 18:19:08 +0200

    refactor: Variabilizado de portfolio-item

--- App/views/portfolio-item.php
+++ App/views/portfolio-item.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="<?=$lang ?>">
+<html lang="<?= $lang ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Branding</title>
-    <meta name="description" content="Diseño sistemas visuales que comunican la personalidad de tu negocio.">
+    <title><?= $title ?></title>
+    <meta name="description" content="<?= $meta_description ?>">
     <?php echo vite_tags('src/js/portfolioItem.js'); ?>
 
     <link rel="canonical" href="<?= $_ENV['RUTA'] ?><?= $url ?>">
@@ -12,7 +12,6 @@
     <meta name="referrer" content="origin">
 
     <?php include $appRoot . '/includes/metadatos_globales.php' ?>
-    
 </head>
 <body>
 
@@ -22,51 +21,27 @@
 
     <header>
         <section>
-
             <article class="art02">
                 <div class="textos">
-                    <h1>Más allá del logotipo</h1>
-                    <p>Diseño sistemas visuales que comunican la personalidad de tu negocio. Selecciono combinaciones que refuerzan el mensaje de la marca e impactan visualmente. Establezco reglas claras de uso para que la comunicación visual sea coherente, profesional y capaz de evocar las emociones adecuadas en el público objetivo.</p>
+                    <h1><?= $hero_title ?></h1>
+                    <p><?= $hero_description ?></p>
                 </div>
             </article>
         </section>
-        
     </header>
 
     <main>
         <section>
-            <!-- art10 -->
             <article class="art10">
+                <?php foreach ($gallery_images as $image) { ?>
                 <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/appdegestion.avif" alt="App de Gestión">
+                    <img src="<?= $_ENV['RUTA'] . $image->src ?>" alt="<?= $image->alt ?>">
                 </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/arrivet.avif" alt="Arrivet">
-                </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/kts.avif" alt="KTS">
-                </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/ori-zautz.avif" alt="Ori-Zautz">
-                </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/teknomai.avif" alt="Teknomai">
-                </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/utilfor.avif" alt="Utilfor">
-                </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/thn.avif" alt="THN">
-                </div>
-                <div>
-                    <img src="<?=$_ENV['RUTA'];?>/assets/img/vistas/portfolio/branding/transformainteriores.avif" alt="Transforma Interiores">
-                </div>
-
+                <?php } ?>
             </article>
         </section>
     </main>
 
-
-    <?php include $appRoot . '/includes/footer.php'?>
+    <?php include $appRoot . '/includes/footer.php' ?>
 </body>
 </html>
